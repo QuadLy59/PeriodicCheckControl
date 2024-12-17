4f4ff07be32ee13a4e7f05faecbd0fd70187d850
QuadLy59 <omerbakarjk@outlook.com>
2024-12-17 17:48:37 +0300

    17.12.2024 son değişiklikler yapıldı

--- Core/PeriodicCheck.Application/obj/Debug/net8.0/PeriodicCheck.Application.AssemblyInfo.cs
+++ Core/PeriodicCheck.Application/obj/Debug/net8.0/PeriodicCheck.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeriodicCheck.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4d33a538ec75cad36a97d3c89bb102629d2aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeriodicCheck.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeriodicCheck.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
